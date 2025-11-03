dc1933dc0fd180950487244e038c31f8bae67270
Rustam Shamsutdinov <sypooit@gmail.com>
2025-11-04 00:46:55 +0300

    done wordcount again)

--- wordcount/main.go
+++ wordcount/main.go
@@ -1,53 +1,50 @@
-package main 
-
+package main
 
 import (
-	"fmt"
 	"bufio"
+	"fmt"
 	"os"
 )
 
-
-
-func helper(err error){
-	if err != nil{
+func helper(err error) {
+	if err != nil {
 		panic(err)
 	}
 }
 
-func closeFile(file *os.File){
+func closeFile(file *os.File) {
 	err := file.Close()
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 
 }
 
-func main(){
+func main() {
 	args := os.Args
 
-	if len(args) < 2{
+	if len(args) < 2 {
 		panic("put paths")
 	}
 
-	lines_map := make(map[string]int)
+	linesMap := make(map[string]int)
 
 	for _, filePath := range args[1:] {
 		file, err := os.Open(filePath)
 		helper(err)
-		
+
 		defer closeFile(file)
 		scanner := bufio.NewScanner(file)
 
-		for scanner.Scan(){
+		for scanner.Scan() {
 			line := scanner.Text()
-			lines_map[line] = lines_map[line] + 1
+			linesMap[line] = linesMap[line] + 1
 		}
-		
+
 		helper(scanner.Err())
 	}
 
-	for line, count := range lines_map {
+	for line, count := range linesMap {
 		if count >= 2 {
 			fmt.Printf("%d\t%s\n", count, line)
 		}
