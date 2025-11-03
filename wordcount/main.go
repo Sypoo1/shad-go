99c0d94203a8c09f77531e876e5484ad9b7098cb
Rustam Shamsutdinov <sypooit@gmail.com>
2025-11-03 19:48:36 +0300

    my wordcount

--- wordcount/main.go
+++ wordcount/main.go
@@ -2,6 +2,34 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func helper(err error){
+	if err != nil{
+		panic(err)
+	}
 }
+
+
+func main() {
+	m := make(map[string]int)
+	var files []string = os.Args[1:]
+	for _, file := range files {
+		data, err := os.ReadFile(file)
+		helper(err)
+		for _, s := range data {
+			ss := string(s)
+			if ss != "\n"{
+				m[ss] = m[ss] + 1
+			}
+		}
+	}
+	for key, value := range m {
+		if value >= 2 {
+			fmt.Printf("%d\t%s\n", value, key)
+		}
+	}
+}	
