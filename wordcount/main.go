7bd272127a6e37b4cf67c9e192cc958f33007486
Rustam Shamsutdinov <sypooit@gmail.com>
2025-11-04 00:27:24 +0300

    my2

--- wordcount/main.go
+++ wordcount/main.go
@@ -1,11 +1,13 @@
-//go:build !solution
+package main 
 
-package main
 
 import (
 	"fmt"
+	"bufio"
 	"os"
 )
+
+
 
 func helper(err error){
 	if err != nil{
@@ -13,23 +15,44 @@ func helper(err error){
 	}
 }
 
+func closeFile(file *os.File){
+	err := file.Close()
+	if err != nil{
+		panic(err)
+	}
 
-func main() {
-	m := make(map[string]int)
-	var files []string = os.Args[1:]
-	for _, file := range files {
-		data, err := os.ReadFile(file)
+}
+
+func main(){
+	args := os.Args
+
+	if len(args) < 2{
+		panic("put paths")
+	}
+
+	lines_map := make(map[string]int)
+
+	for _, filePath := range(args){
+		file, err := os.Open(filePath)
 		helper(err)
-		for _, s := range data {
-			ss := string(s)
-			if ss != "\n"{
-				m[ss] = m[ss] + 1
-			}
+		
+		defer closeFile(file)
+		scanner := bufio.NewScanner(file)
+
+		for scanner.Scan(){
+			line := scanner.Text()
+			lines_map[line] = lines_map[line] + 1
+		}
+		
+		// err = scanner.Err()
+		// if err != nil{
+		// 	panic(err)
+		// }
+	}
+
+	for line, count := range lines_map {
+		if count >= 2 {
+			fmt.Printf("%d\t%s\n", count, line)
 		}
 	}
-	for key, value := range m {
-		if value >= 2 {
-			fmt.Printf("%d\t%s\n", value, key)
-		}
-	}
-}	
+}
