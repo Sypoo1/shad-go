102d508fe7ff8f6442647b4b1ee85aef491d6e3b
Rustam Shamsutdinov <sypooit@gmail.com>
2025-11-07 01:37:50 +0300

    finally done!

--- hotelbusiness/hotels.go
+++ hotelbusiness/hotels.go
@@ -2,6 +2,10 @@
 
 package hotelbusiness
 
+import (
+	"slices"
+)
+
 type Guest struct {
 	CheckInDate  int
 	CheckOutDate int
@@ -13,5 +17,30 @@ type Load struct {
 }
 
 func ComputeLoad(guests []Guest) []Load {
-	return []Load{}
+	load := []Load{}
+	if len(guests) == 0 {
+		return load
+	}
+	events := make(map[int]int)
+	for _, guest := range guests {
+		events[guest.CheckInDate]++
+		events[guest.CheckOutDate]--
+	}
+
+	dates := []int{}
+	for date :=  range events {
+		dates = append(dates, date)
+	}
+	slices.Sort(dates)
+	cnt := 0
+	for _, date := range dates {
+		if events[date] == 0 {
+			continue
+		}
+		cnt += events[date]
+		load = append(load, Load{date, cnt})
+	}
+
+
+	return load
 }
